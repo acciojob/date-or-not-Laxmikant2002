9c6f40c49d6aee263d16fee982135847485f3ae1
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-10-05 19:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var isDate = function (input) {
   //   write your code here
+  if (input instanceof Date && !isNaN(input.getTime())) return true;
+  // Try to parse input as a date
+  let date = new Date(input);
+  // Check if parsing gives a valid date
+  return !isNaN(date.getTime());
 };
 
 // Do not change the code below.
